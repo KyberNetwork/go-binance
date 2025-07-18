aa76204451a01823e49857991beef98035afeec2
Haiss2 <hai98th@gmail.com>
2025-07-18 16:55:49 +0700

    Feature/Directly_ping_ws_connection

--- v2/websocket_service.go
+++ v2/websocket_service.go
@@ -19,7 +19,8 @@ var (
 	BaseWsApiTestnetURL    = "wss://testnet.binance.vision/ws-api/v3"
 
 	// WebsocketTimeout is an interval for sending ping/pong messages if WebsocketKeepalive is enabled
-	WebsocketTimeout = time.Second * 60
+	WebsocketTimeout      = time.Second * 60
+	KeepAlivePingDeadline = time.Second * 10
 	// WebsocketPongTimeout is an interval for sending a PONG frame in response to PING frame from server
 	WebsocketPongTimeout = time.Second * 10
 	// WebsocketKeepalive enables sending ping/pong messages to check the connection stability
